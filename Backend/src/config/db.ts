c4960f76c4be2a400a0c21d65939f5ff8c4e9d3c
seba25 <ricardo250297@gmail.com>
2025-12-23 09:31:51 -0300

    log db 9.31

--- Backend/src/config/db.ts
+++ Backend/src/config/db.ts
@@ -2,6 +2,14 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log({
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_USER: process.env.DB_USER,
+  DB_NAME: process.env.DB_NAME,
+});
+
+
 export const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
